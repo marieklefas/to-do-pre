3d28bd6fc5879ef66b134b282d7c0508a66b0430
Marie <klefasmarie@gmail.com>
2026-03-28 03:04:55 +0300

    Done

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
+	if (localStorage.getItem("tasks")) {
+		return JSON.parse(localStorage.getItem("tasks"));
+	}
 	return items;
 }
 
@@ -24,19 +27,59 @@ function createItem(item) {
   const editButton = clone.querySelector(".to-do__item-button_type_edit");
 
 	textElement.textContent = item;
+
+	deleteButton.addEventListener("click", () => {
+		clone.remove(item);
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	})
+
+	duplicateButton.addEventListener("click", () => {
+		const itemName = textElement.textContent;
+		const newItem = createItem(itemName);
+		listElement.prepend(newItem);
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	})
+
+	editButton.addEventListener("click", () => {
+		textElement.setAttribute("contenteditable", true);
+		textElement.focus();
+	})
+
+	textElement.addEventListener("blur", () => {
+		textElement.setAttribute("contenteditable", false);
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	})
+
 	return clone;
 }
 
 function getTasksFromDOM() {
-
+	const itemsNamesElements = document.querySelectorAll(".to-do__item-text");
+	const tasks = [];
+	itemsNamesElements.forEach(function (el) {
+		tasks.push(el.textContent)
+	})
+	return tasks;
 }
 
 function saveTasks(tasks) {
-
+	localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 items = loadTasks();
 items.forEach(item => {
 	newItem = createItem(item);
 	listElement.append(newItem)
+})
+
+formElement.addEventListener("submit", (event) => {
+	event.preventDefault();
+	let items = createItem(inputElement.value)
+	listElement.prepend(items);
+	items = getTasksFromDOM();
+	saveTasks(items);
+	formElement.reset();
 })
